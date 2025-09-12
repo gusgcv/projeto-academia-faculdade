5d9f76e59560a355da68f25fe15d8c9145c26193
gusgcv <gustavogsdev@gmail.com>
2025-09-12 19:39:51 -0300

    atualização 1

--- inicio.py
+++ inicio.py
@@ -1,6 +1,22 @@
 import sqlite3
 from flask import Flask, render_template_string, request, redirect, url_for, g
-
+ 
+# Flask # 
 app = Flask(__name__)
 DATABASE = 'academia.db'
 
+#Base banco de dados em SQlite#
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g. _database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app .teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def init_db():
